3a8d6faa66883d72d04202750dc627e2fd8b668f
JP Valera <johnpatrickyusoresvalera@gmail.com>
2020-06-29 16:09:11 +0800

    Updates

---
+++ precache-manifest.72c39ab06ff2f3cd1e1ee9df6475584a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b9241868e8c0ef1849dc8bdaa4a0ec",
+    "url": "/track-covid19/index.html"
+  },
+  {
+    "revision": "71205dc8b40cf6758d50",
+    "url": "/track-covid19/static/js/2.aae20d00.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/track-covid19/static/js/2.aae20d00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ec71172403f79ed2e3",
+    "url": "/track-covid19/static/js/main.6c79ecca.chunk.js"
+  },
+  {
+    "revision": "9a9a0a23efeb79c4ea61",
+    "url": "/track-covid19/static/js/runtime-main.268def31.js"
+  }
+]);
